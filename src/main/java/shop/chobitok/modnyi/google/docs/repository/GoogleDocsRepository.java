5f66eda6897ad989b724270e7b130f524611d9a7
Volodymyr Horodetskyi <volodymyr.horodetskyi@ppb.com>
2020-11-12 16:17:58 +0200

    set another port in google docs repo

--- src/main/java/shop/chobitok/modnyi/google/docs/repository/GoogleDocsRepository.java
+++ src/main/java/shop/chobitok/modnyi/google/docs/repository/GoogleDocsRepository.java
@@ -59,7 +59,7 @@ public class GoogleDocsRepository {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8887).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
