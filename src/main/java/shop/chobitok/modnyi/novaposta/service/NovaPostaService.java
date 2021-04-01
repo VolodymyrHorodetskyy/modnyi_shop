87410a63963a36f10c4e7f02d2482894ffa67e16
vhorodetskyi <horodetskyyv@gmail.com>
2021-04-01 13:09:45 +0300

    increased number of days for list tracking entity

--- src/main/java/shop/chobitok/modnyi/novaposta/service/NovaPostaService.java
+++ src/main/java/shop/chobitok/modnyi/novaposta/service/NovaPostaService.java
@@ -41,7 +41,7 @@ public class NovaPostaService {
             //if status created
             if (data.getStatusCode().equals(3) || ShoeUtil.convertToStatus(data.getStatusCode()) == Status.СТВОРЕНО) {
                 Ordered ordered1 = npOrderMapper.toOrdered(
-                        postaRepository.getDataForListFromListTrackingEntityInFiveDaysPeriod(ttn, 5,
+                        postaRepository.getDataForListFromListTrackingEntityInFiveDaysPeriod(ttn, 10,
                                 ordered != null ? ordered.getNpAccountId() : null),
                         discount);
                 if (ordered1 != null) {
