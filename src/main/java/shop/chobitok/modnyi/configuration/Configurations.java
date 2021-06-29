ce756e1771956ba24d491f1ffe3ea0ebde6ee6d1
vhorodetskyi <horodetskyyv@gmail.com>
2021-06-29 18:59:23 +0300

    removed timezone to hibernate and to connection to db url added timezone to datetime provider

--- src/main/java/shop/chobitok/modnyi/configuration/Configurations.java
+++ src/main/java/shop/chobitok/modnyi/configuration/Configurations.java
@@ -1,19 +1,29 @@
 package shop.chobitok.modnyi.configuration;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Optional;
 
 @Configuration
+@EnableJpaAuditing
 public class Configurations extends PropertyNamingStrategy {
 
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-//        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
         return mapper;
+    }
+
+    @Bean
+    public DateTimeProvider utcDateTimeProvider() {
+        return () -> Optional.of(LocalDateTime.now(ZoneOffset.of("UTC+3")));
     }
 }
 
