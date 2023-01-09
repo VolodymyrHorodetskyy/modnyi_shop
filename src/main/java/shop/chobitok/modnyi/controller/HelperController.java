844a99da4cef44f1c3805bbca25ab59c17f1435b
admin <horodetskyyv@gmail.com>
2023-01-09 16:13:27 +0200

    helper controller added

--- src/main/java/shop/chobitok/modnyi/controller/HelperController.java
+++ src/main/java/shop/chobitok/modnyi/controller/HelperController.java
@@ -1,9 +1,6 @@
 package shop.chobitok.modnyi.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shop.chobitok.modnyi.service.StorageService;
 
 import static java.lang.System.out;
@@ -15,10 +12,10 @@ public class HelperController {
 
     private StorageService storageService;
 
-    @GetMapping("getAvailableFromStorage")
-    public String getAvailableSizesFromStorage() {
+    @GetMapping("getAvailableFromStorage/{shoeId}")
+    public String getAvailableSizesFromStorage(@PathVariable Long shoeId) {
         StringBuilder stringBuilder = new StringBuilder();
-        storageService.getStorageRecords(17950l, null, null, null, true)
+        storageService.getStorageRecords(shoeId, null, null, null, true)
                 .forEach(storageRecord -> {
                     stringBuilder.append(storageRecord.getShoe().getModelAndColor() + " " + storageRecord.getSize() + " " + storageRecord.getComment());
                 });
