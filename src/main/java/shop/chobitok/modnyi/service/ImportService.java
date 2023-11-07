08970037f886dcdc2ae4c22c7c047113f52f0b88
admin <horodetskyyv@gmail.com>
2023-11-07 20:44:07 +0200

    fix

--- src/main/java/shop/chobitok/modnyi/service/ImportService.java
+++ src/main/java/shop/chobitok/modnyi/service/ImportService.java
@@ -78,7 +78,9 @@ public class ImportService {
         } else if (isEmpty(ordered.getPostComment())) {
             result = "Не вдалось витягнути комент з НП апі";
         }
-        result += ", статус накландої = " + ordered.getStatus();
+        if (!isEmpty(result)) {
+            result += ", статус накландої = " + ordered.getStatus();
+        }
         return result;
     }
 }
