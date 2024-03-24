5cc963eca8eba9673b9113659670556e47cdaaad
admin <horodetskyyv@gmail.com>
2024-03-24 18:48:04 +0200

    зміна юзера і токена для телеграм бота

--- src/main/java/shop/chobitok/modnyi/telegram/ChobitokBot.java
+++ src/main/java/shop/chobitok/modnyi/telegram/ChobitokBot.java
@@ -35,12 +35,12 @@ public class ChobitokBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "mchobitok_testing_bot";
+        return "mchobitok_bot";
     }
 
     @Override
     public String getBotToken() {
-        return "6421822261:AAEtyjfrdkYPcNvBuGNHK6VvLRPz_beNSpI";
+        return "5877883629:AAENWWaRFn_Mn31OumRIENAHxh-Albajvy4";
     }
 
     @Override
